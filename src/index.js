98e8fd6e96f5f5fe6bac0538be64821519f2ded1
pixan198 <ashish11chawda@gmail.com>
2020-09-25 04:42:03 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
-
+//import App from './App';
+import ResponsiveDrawer from './components/Drawer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    
+    <ResponsiveDrawer />
   </React.StrictMode>,
   document.getElementById('root')
 );
